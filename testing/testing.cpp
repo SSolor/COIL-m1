d3248d98293b77c69419d3af85c230001d02b2b5
sebastian <sebastianhsolorzano@gmail.com>
2026-03-24 15:05:58 -0400

    added testing solution

---
+++ testing/testing.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testing
+{
+	TEST_CLASS(testing)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
