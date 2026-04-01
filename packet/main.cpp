c421898982cb398b182a377ae78a771dead0c638
sebastian <sebastianhsolorzano@gmail.com>
2026-04-01 01:38:56 -0400

    socket is hopefully problem free

--- packet/main.cpp
+++ packet/main.cpp
@@ -21,7 +21,7 @@ int main(void) {
 	turn.Direction = RIGHT;
 	turn.Duration = 12;
 
-	//p.SetBodyData((char*) & turn, sizeof(turn));
+	//p.SetBodyData((char*) & drive, sizeof(drive));
 	char* x = p.GenPacket();
 	printf("our packet: %d\n", &x);
 	for (int i = 0;i < p.GetLength();i++) {
@@ -29,16 +29,18 @@ int main(void) {
 	}
 	printf("\n");
 
-	
-	WSADATA wsaData;
-	if ((WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
-		return -1;
-	}
-	/*
 
+	if(ONWINDOWS){
+		WSADATA wsaData;
+		if ((WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
+			return -1;
+		}
+	}
+	
+	/*
 	//initializes socket. SOCK_STREAM: TCP
 	SOCKET ClientSocket;
-	ClientSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (ClientSocket == INVALID_SOCKET) {
 		WSACleanup();
 		return -1;
@@ -47,25 +49,52 @@ int main(void) {
 	//Connect socket to specified server
 	sockaddr_in SvrAddr;
 	SvrAddr.sin_family = AF_INET;						//Address family type itnernet
-	SvrAddr.sin_port = htons(29500);					//port (host to network conversion)
+	SvrAddr.sin_port = htons(29000);					//port (host to network conversion)
 	SvrAddr.sin_addr.s_addr = inet_addr("10.172.41.150");
 
-	char recv[150];
-	int SaddrLen = sizeof(SvrAddr); //needed by rcvfrom for some reason
+	if ((connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr))) == SOCKET_ERROR) {
+		//I was having some issues at this stage so I added this
 
-
-	sendto(ClientSocket, x, p.GetLength(), 0, (sockaddr*)&SvrAddr, sizeof(SvrAddr));
-	int a=recvfrom(ClientSocket, recv, 150, 0, (struct sockaddr*)&SvrAddr, &SaddrLen);
-	printf("%d\n", a);
-	for (int i = 0; i < a;i++) {
-		printf("%d,",recv[i]);
+		closesocket(ClientSocket);
+		return -1;
 	}
 
-	PktDef r(recv);
+	char recvi[150];
+	send(ClientSocket, x, p.GetLength(), 0);
+	int a = recv(ClientSocket, recvi, 150, 0);
+	//int SaddrLen = sizeof(SvrAddr); //needed by rcvfrom for some reason
+	printf("%d\n", a);
+	for (int i = 0; i < a;i++) {
+		printf("%d,", recvi[i]);
+	}
+
+	PktDef r(recvi);
 	printf("\nwe got: ack: %d, cmd: %d, count: %d, %s\n", r.GetAck(), r.GetCmd(), r.GetPktCount(), r.GetBodyData());
+
+	printf("--------\n");
+
+	x[0] = 9;
+	char revii[150];
+
+	send(ClientSocket, x, p.GetLength(), 0);
+	int b = recv(ClientSocket, revii, 150, 0);
+	//int SaddrLen = sizeof(SvrAddr); //needed by rcvfrom for some reason
+	printf("%d\n", b);
+	for (int i = 0; i < b;i++) {
+		printf("%d,", revii[i]);
+	}
+	PktDef rI(revii);
+	printf("\nwe got: ack: %d, cmd: %d, count: %d, %s\n", rI.GetAck(), rI.GetCmd(), rI.GetPktCount(), rI.GetBodyData());
+	
+
+	//sendto(ClientSocket, x, p.GetLength(), 0, (sockaddr*)&SvrAddr, sizeof(SvrAddr));
+	//int a=recvfrom(ClientSocket, recvi, 150, 0, (struct sockaddr*)&SvrAddr, &SaddrLen);
 	*/
+
+	
+	
 		MySocket soc(CLIENT, "10.172.41.150", 29000, TCP, 100);
-		soc.ConnectTCP();
+		soc.SetCType(TCP);
 		soc.ConnectTCP();
 
 		//soc.ConnectTCP();
@@ -81,10 +110,16 @@ int main(void) {
 		PktDef r(rc);
 		printf("\nwe got: ack: %d, cmd: %d, count: %d, %s\n", r.GetAck(), r.GetCmd(), r.GetPktCount(), r.GetBodyData());
 
+
 		printf("--------\n");
+		soc.DisconnectTCP();
+		soc.SetIPAddr("10.172.41.150");
+		soc.SetType(SERVER);
+		soc.resetSocket();
+		soc.ConnectTCP();
+
 		x[0] = 9;
 		soc.SendData(x, p.GetLength());
-
 		char rc2[150];
 		int forlan = soc.GetData(rc2);
 		printf("%d\n", forlan);
@@ -93,6 +128,9 @@ int main(void) {
 		}
 		PktDef rfo(rc2);
 		printf("\nwe got: ack: %d, cmd: %d, count: %d, %s\n", rfo.GetAck(), rfo.GetCmd(), rfo.GetPktCount(), rfo.GetBodyData());
+
+
+		
 		
 	/*
 	SocketType stype = CLIENT;
--- packet/socket.h
+++ packet/socket.h
@@ -2,6 +2,7 @@
 
 #pragma once
 #ifdef _WIN32
+
 	#define _WINSOCK_DEPRECATED_NO_WARNINGS
 	#include <winsock2.h>
 	#include <ws2tcpip.h> 
@@ -9,13 +10,15 @@
 	//but I couldn't get sockaddrlen to work wiht that
 	#pragma comment(lib, "Ws2_32.lib")
 	#define ONWINDOWS 1 //for some reason, having it as 'true' causes issues in ifs
-	//int //close(SOCKET s) { return 0; }
+	//int //close(SOCKET s) { return 0; } //this causes issues for some reason
+
 #elif __linux__ 
 	#include <unistd.h>
 	#include <sys/socket.h>
 	#include <netinet/in.h>
 
 	#include <arpa/inet.h> //needed for inet_addr()
+	#include <cstring>
 	#define INVALID_SOCKET ~0 //this is how ONWINDOWS does it idk man
 	#define SOCKET_ERROR -1
 	#define SOCKET int
@@ -35,7 +38,7 @@ typedef enum ConnectionType {
 	TCP,
 	UDP
 };
-const int DEFAULT_SIZE = 0;
+const int DEFAULT_SIZE = 5;//i'm not sure exactly, but is should at least hold an empty packet, right?
 
 class MySocket {
 	char* Buffer;
@@ -50,12 +53,16 @@ class MySocket {
 	struct sockaddr_in SvrAddr;
 	//udp needs the following
 	struct sockaddr_in CliAddr;
-	int AddrLen;
+	int AddrLen;//I think it's fine if server and client share this
 
 	SocketType mySocket;
 	ConnectionType connectionType;
 
+	bool welcomeopen;
+	bool changesmade;
 
+
+	//initializes sockets based on stored values
 	int socketStart() {
 
 
@@ -94,8 +101,9 @@ class MySocket {
 				if (ONWINDOWS) {
 					closesocket(ConnectionSocket);
 				}
-				else
+				else {
 					//close(ConnectionSocket);
+				}
 				return -1;
 			}
 
@@ -116,8 +124,9 @@ class MySocket {
 			if (bind(WelcomeSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr)) == SOCKET_ERROR) {
 				if (ONWINDOWS) 
 					closesocket(WelcomeSocket);
-				else
+				else {
 					//close(WelcomeSocket);
+				}
 				return -1;
 			}
 
@@ -125,11 +134,12 @@ class MySocket {
 			if (listen(WelcomeSocket, 1) == SOCKET_ERROR) { //I think 1 is fine here?
 				if (ONWINDOWS)
 					closesocket(WelcomeSocket);
-				else
+				else {
 					//close(WelcomeSocket);
+				}
 				return -1;
 			}
-
+			welcomeopen = true;
 			//tcp server is able to accept() from this stage
 			return 0;
 		}
@@ -149,6 +159,7 @@ public:
 		SvrAddr;
 		CliAddr;
 		AddrLen = sizeof(SvrAddr);
+		bTCPConnect = false;
 		
 		if (size > DEFAULT_SIZE) {
 			Buffer = new char[size];
@@ -161,7 +172,8 @@ public:
 
 		int ok=socketStart();
 		if (ok != 0) {
-			exit(EXIT_FAILURE);//can't return on a constructor, could this be an exception instead?
+			fprintf(stderr, "socket or bind failed\n");
+			return;
 		}
 	}
 	~MySocket() {
@@ -171,20 +183,20 @@ public:
 	}
 
 	void ConnectTCP() {
-		if (connectionType == UDP || bTCPConnect == true) {
+		if (connectionType == UDP || bTCPConnect == true || changesmade==true) {
 			//I think for all these writing to stderr is acceptable? unless there's a cpp way of doing it
-			fprintf(stderr, "can't connect like this\n");
+			fprintf(stderr, "UDP or already connected or changes made\n");
 			return;
 		}
 		else {
 			if (mySocket == CLIENT) {
-
 				if (connect(ConnectionSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr)) == SOCKET_ERROR) {
 					if (ONWINDOWS)
 						closesocket(ConnectionSocket);
-					else
+					else {
 						//close(ConnectionSocket);
-					fprintf(stderr, "failed to connect\n");
+					}
+					fprintf(stderr, "tcp failed to connect\n");
 					return;
 				}
 				bTCPConnect = true;
@@ -195,9 +207,10 @@ public:
 				if ((ConnectionSocket = accept(WelcomeSocket, NULL, NULL)) == SOCKET_ERROR) {
 					if (ONWINDOWS)
 						closesocket(WelcomeSocket);
-					else 
+					else {
 						//close(WelcomeSocket);
-					fprintf(stderr, "failed to accept\n");
+					}
+					fprintf(stderr, "tcp failed to accept\n");
 					return;
 				}
 				bTCPConnect = true;
@@ -214,88 +227,138 @@ public:
 		else{
 			if(ONWINDOWS)
 				closesocket(ConnectionSocket);
-			else
+			else {
 				//close(ConnectionSocket);
-
+			}
 			//I'm assuming this function doesn't want to 'end everything',
 			//so I'm not closing welcomesocket or anything
-			bTCPConnect == false;
+			bTCPConnect = false;
+
+			int okay=socketStart();//gets killed socket ready for connection again
+			if (okay != 0)
+				fprintf(stderr, "new socket failed\n");
+			//if you want to change things about your socket, after doing this, set<whatever>(), resetsocket();
+			//having it like this prevents double closing or anything
 
 			return;
 		}
 	}
+	void resetSocket() {
+		if (ONWINDOWS)
+			closesocket(ConnectionSocket);
+		else {
+			//close(ConnectionSocket);
+		}
+		int okay=socketStart();
+		if (okay != 0)
+			fprintf(stderr, "new socket failed\n");
+		changesmade = false;
+	}
 	void KillTCPServ() {
-		if (connectionType == TCP && bTCPConnect == false && mySocket == SERVER) {
+		if (connectionType == TCP && bTCPConnect == false && mySocket == SERVER &&welcomeopen==true) {
 			if (ONWINDOWS)
 				closesocket(WelcomeSocket);
-			//else
+			else {
 				//close(WelcomeSocket);
+			}
+			welcomeopen = false;
 		}
 		else {
-			fprintf(stderr, "you cannot do that\n");
+			fprintf(stderr, "you cannot kill server now\n");
 		}
 		return;
 	}
 
 	void SendData(const char* dat, int size) {
+		if (changesmade == true) {
+			fprintf(stderr, "cannot send data, changes have been made\n");
+			return;
+		}
+
 		if (connectionType == TCP && bTCPConnect == true) {
 			//dont want to call this without having connected
 			send(ConnectionSocket, dat, size, 0);
 		}
 		else if (connectionType == UDP && mySocket==CLIENT) {
-			int res=sendto(ConnectionSocket, dat, size, 0, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr));
-			printf("size send: %d\n", res);
+			sendto(ConnectionSocket, dat, size, 0, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr));
 		}
 		else if (connectionType == UDP && mySocket == SERVER) {
 			sendto(ConnectionSocket, dat, size, 0, (struct sockaddr*)&CliAddr, AddrLen);
+			printf("sent in servermode\n");
 		}
 		else
-			fprintf(stderr, "cannot do that right now\n");
+			fprintf(stderr, "cannot send data now\n");
 		return;
 	}
 	int GetData(char* buf) {
+		if (changesmade == true) {
+			fprintf(stderr, "cannot get data, changes have been made\n");
+			return -1;
+		}
+
 		int recsize=0;
 		if (connectionType == TCP && bTCPConnect == true) {
 			recsize=recv(ConnectionSocket, Buffer, MaxSize, 0);
 			//if the send data is bigger than maxsize, this might be a problem, but CRCs will tell us to throw away bad packets anyways
-			printf("at tcp\n");
 		}
 		else if (connectionType == UDP && mySocket == CLIENT) {
 			recsize = recvfrom(ConnectionSocket, Buffer, MaxSize, 0, (struct sockaddr*)&SvrAddr, &AddrLen);
-			printf("recieved, size: %d\n", recsize);
 		}
 		else if (connectionType == UDP && mySocket == SERVER) {
 			recsize = recvfrom(ConnectionSocket, Buffer, MaxSize, 0, (struct sockaddr*)&CliAddr, &AddrLen);
-			printf("at udp serv\n");
 		}
-
-		if (recsize < 0)
+		//happens when recv/from is invalid
+		if (recsize <= 0)
 			return -1;
+
 		memcpy(buf, Buffer, recsize);
 		return recsize;
 	}
+
 	std::string GetIPAddr() {return IPAddr;}
+	//will not be reflected until reset
 	void SetIPAddr(std::string newaddr) {
-		if (bTCPConnect == true)
-			fprintf(stderr, "you cannot change this right now!\n");
+		if (bTCPConnect == true || welcomeopen==true)
+			fprintf(stderr, "you cannot change ip right now!\n");
 		else
 			IPAddr = newaddr;
 		return;
 	}
+	//will not be reflected until reset
 	void SetPort(unsigned int newport) {
-		if (bTCPConnect == true)
-			fprintf(stderr, "you cannot do this right now!\n");
+		if (bTCPConnect == true ||welcomeopen==true)
+			fprintf(stderr, "you cannot do port right now!\n");
 		else
 			Port = newport;
 		return;
 	}
 	int GetPort() {return Port;}
+
 	SocketType GetType() {return mySocket;}
+	//will block certain functionality until reset
 	void SetType(SocketType newtype) {
-		if (bTCPConnect == true)
-			fprintf(stderr, "you cannot change this right now!\n");
-		else 
+		if (bTCPConnect == true ||welcomeopen == true)
+			fprintf(stderr, "you cannot change socketype right now!\n");
+		else if (mySocket == newtype)
+			return;
+		else {
 			mySocket = newtype;
+			changesmade = true;
+		}
+		return;
+	}
+
+	ConnectionType GetCType() { return connectionType; }
+	//will block certain functionality until reset
+	void SetCType(ConnectionType newtype) {
+		if (bTCPConnect == true || welcomeopen == true)
+			fprintf(stderr, "you cannot change connectiontype right now!\n");
+		else if (connectionType == newtype)
+			return;
+		else {
+			connectionType = newtype;
+			changesmade = true;
+		}
 		return;
 	}
 };
